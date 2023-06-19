68e6d3df6db2ef3f7ff922a3c1d6506508619186
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-06-20 01:09:04 +0600

    complete the  banner section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import Profile from "../../assets/home.jpg"
+import { Link } from 'react-router-dom';
+import {FaArrowRight} from "react-icons/fa";
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="home section grid">
+            <img src={Profile} alt="" className="home__img" />
+
+            <div className="home__content">
+                <div className="home__data">
+                    <h1 className="home__title"><span>I'm Sakib Hasan.</span> Web Developer</h1>
+
+                    <p className="home__description">
+                        I'm a Tunisian based web developer & front-end developer focused on crafting clean & user-friendly experiences, I am passionate about building excellent software that improves the lives of those around me. 
+                    </p>
+
+                    <Link to='/about' className='button'>
+                        More About Me {' '}
+                        <span className='button__icon'>
+                            <FaArrowRight/>
+                        </span>
+                    </Link>
+                </div>
+            </div>
+
+            <div className="color__block"></div>
+        </section>
     );
 };
 
