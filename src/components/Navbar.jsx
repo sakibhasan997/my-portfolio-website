7be54205bd7be93a479e5a17fc2591ba6c1a2bd2
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-06-19 21:22:23 +0600

    create my css framework

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { links } from '../data';
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className='nav'>
+            <div className="nav__menu">
+                <ul className="na__list">
+                    {links.map(({name, icon, path}, index) => {
+                        return(
+                            <li className="nav__item" key={index}>
+                                
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        </nav>
     );
 };
 
