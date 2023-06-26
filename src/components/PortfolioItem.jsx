42cd4be0161d58dfe2a539f5a03985fb35df17b4
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-06-26 13:37:35 +0600

    project section is done

---
+++ src/components/PortfolioItem.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import Close from "../assets/close.svg"
+
+const PortfolioItem = ({ img, title, details }) => {
+
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal)
+    };
+
+    return (
+        <div className="portfolio__item">
+            <img className='portfolio__img' src={img} alt="" />
+
+            <div className="portfolio__hover" onClick={toggleModal}>
+                <h3 className="portfolio__title">{title}</h3>
+            </div>
+
+            {modal && (
+                <div className="portfolio__modal">
+                    <div className="portfolio__modal-content">
+                        <img className='modal__close' src={Close} alt="" onClick={toggleModal} />
+
+                        <h3 className="modal__title">{title}</h3>
+
+                        <ul className="modal__list grid">
+                            {details.map(({ icon, title, desc }, index) => {
+                                return (
+                                    <li className="modal__item" key={index} >
+                                        <span className="item__icon">{icon}</span>
+
+                                        
+                                            <span className="item__title">{title}</span>
+                                            <div className="item__details">{desc}</div>
+                                        
+                                    </li>
+                                )
+                            })}
+                        </ul>
+
+                        <img src={img} className='modal__img' alt="" />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default PortfolioItem;
